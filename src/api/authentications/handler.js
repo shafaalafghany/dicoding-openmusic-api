ee2fa21ce790f122de1e721fbac7bb3912e285bf
Shafa Alafghany <alafghanyshafa@gmail.com>
2022-05-22 21:35:47 +0800

    refactor: deleted repeated code on authentications

--- src/api/authentications/handler.js
+++ src/api/authentications/handler.js
@@ -1,7 +1,5 @@
-/* eslint-disable consistent-return */
 /* eslint-disable no-underscore-dangle */
-const ClientError = require('../../exceptions/ClientError');
-const { SUCCESS, ERROR } = require('../../utils/constant');
+const { SUCCESS } = require('../../utils/constant');
 
 class AuthenticationsHandler {
   constructor(authenticationsService, usersService, tokenManager, validator) {
@@ -16,59 +14,36 @@ class AuthenticationsHandler {
   }
 
   async postAuthenticationHandler(req, res) {
-    try {
-      this._validator.validatePostAuthenticationPayload(req.payload);
+    this._validator.validatePostAuthenticationPayload(req.payload);
 
-      const userId = await this._usersService.verifyUserCredential(req.payload);
-      const accessToken = this._tokenManager.generateAccessToken({ userId });
-      const refreshToken = this._tokenManager.generateRefreshToken({ userId });
+    const userId = await this._usersService.verifyUserCredential(req.payload);
+    const accessToken = this._tokenManager.generateAccessToken({ userId });
+    const refreshToken = this._tokenManager.generateRefreshToken({ userId });
 
-      await this._authenticationsService.addRefreshToken(refreshToken);
+    await this._authenticationsService.addRefreshToken(refreshToken);
 
-      return SUCCESS(res, 201, 'success', 'add new token successful', { accessToken, refreshToken });
-    } catch (error) {
-      if (error instanceof ClientError) {
-        return ERROR(res, error.statusCode, 'fail', error.message);
-      }
-
-      return ERROR(res, error.statusCode, 'error', error.message);
-    }
+    return SUCCESS(res, 201, 'success', 'add new token successful', { accessToken, refreshToken });
   }
 
   async putAuthenticationHandler(req, res) {
-    try {
-      this._validator.validatePutAuthenticationPayload(req.payload);
-      const { refreshToken } = req.payload;
-      await this._authenticationsService.verifyRefreshToken(refreshToken);
-      const { userId } = await this._tokenManager.verifyRefreshToken(refreshToken);
-      const accessToken = await this._tokenManager.generateAccessToken({ userId });
+    this._validator.validatePutAuthenticationPayload(req.payload);
+    const { refreshToken } = req.payload;
 
-      return SUCCESS(res, 200, 'success', 'update token successful', { accessToken });
-    } catch (error) {
-      if (error instanceof ClientError) {
-        return ERROR(res, error.statusCode, 'fail', error.message);
-      }
+    await this._authenticationsService.verifyRefreshToken(refreshToken);
+    const { userId } = await this._tokenManager.verifyRefreshToken(refreshToken);
+    const accessToken = await this._tokenManager.generateAccessToken({ userId });
 
-      return ERROR(res, error.statusCode, 'error', error.message);
-    }
+    return SUCCESS(res, 200, 'success', 'update token successful', { accessToken });
   }
 
   async deleteAuthenticationHandler(req, res) {
-    try {
-      this._validator.validateDeleteAuthenticationPayload(req.payload);
+    this._validator.validateDeleteAuthenticationPayload(req.payload);
 
-      const { refreshToken } = req.payload;
-      await this._authenticationsService.verifyRefreshToken(refreshToken);
-      await this._authenticationsService.deleteRefreshToken(refreshToken);
+    const { refreshToken } = req.payload;
+    await this._authenticationsService.verifyRefreshToken(refreshToken);
+    await this._authenticationsService.deleteRefreshToken(refreshToken);
 
-      return SUCCESS(res, 200, 'success', 'delete token successful');
-    } catch (error) {
-      if (error instanceof ClientError) {
-        return ERROR(res, error.statusCode, 'fail', error.message);
-      }
-
-      return ERROR(res, error.statusCode, 'error', error.message);
-    }
+    return SUCCESS(res, 200, 'success', 'delete token successful');
   }
 }
 
